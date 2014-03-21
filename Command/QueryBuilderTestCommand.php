28fb25c8f6a4af33dd218fcfa23a930842058325
Bertrand Dunogier <bd@ez.no>
2014-03-21 04:14:28 +0100

    Added test command
    Run using php ezpublish/console query-builder:test.
    Edit query in vendor/ezsystems/query-builder-bundle/EzSystems/QueryBuilderBundle/Command/QueryBuilderTestCommand.php

---
+++ Command/QueryBuilderTestCommand.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * File containing the TestInitDbCommand class.
+ *
+ * @copyright Copyright (C) 2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace EzSystems\QueryBuilderBundle\Command;
+
+use eZ\Publish\API\Repository\QueryBuilder;
+use eZ\Publish\API\Repository\Values\Content\Content;
+use eZ\Publish\API\Repository\Values\Content\Query;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\TableHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class QueryBuilderTestCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName( 'query-builder:test' );
+    }
+
+    protected function execute( InputInterface $input, OutputInterface $output )
+    {
+
+        // home: 45.7093779, 4.812085799999977
+        // office: 45.76272282060459, 4.835738895106488
+        // sections: media = 3, content = 1
+        // checkbox: vip
+        
+        $queryBuilder = $this->getQueryBuilder();
+        $queryBuilder
+            ->sortBy()->contentName()->desc();
+        $query = $queryBuilder->getQuery();
+
+        $this->printSearchResults( $output, $query );
+    }
+
+    /** 
+     * @return \EzSystems\QueryBuilderBundle\eZ\Publish\API\QueryBuilder
+     */
+    private function getQueryBuilder()
+    {
+        return $this->getContainer()->get( 'ezpublish.api.query_builder' );
+    }
+    
+    private function printSearchResults( OutputInterface $output, Query $query )
+    {
+        $searchService = $this->getContainer()->get( 'ezpublish.api.service.search' );
+        $results = $searchService->findContent( $query );
+
+        if ( $results->totalCount === 0 )
+        {
+            $output->writeln( "No results" );
+            return;
+        }
+
+        $output->writeln( '' );
+        $output->writeln( "<options=bold>{$results->totalCount} results</options=bold>" );
+        $output->writeln( '' );
+        $table = $this->getTableHelper();
+        $table->setHeaders( array( 'Content ID', 'Name', 'Type' ) );
+        foreach ( $results->searchHits as $searchHit )
+        {
+            /** @var Content $content */
+            $content = $searchHit->valueObject;
+
+            $table->addRow(
+                array(
+                    $content->id,
+                    $content->contentInfo->name,
+                    $this->getContentTypeIdentifier( $content->contentInfo->contentTypeId )
+                )
+            );
+        }
+        $table->render( $output );
+    }
+
+    /**
+     * @return TableHelper
+     */
+    private function getTableHelper()
+    {
+        return $this->getHelperSet()->get( 'table' );
+    }
+
+    private function getContentTypeIdentifier( $contentTypeId )
+    {
+        static $contentTypeIdIdentifierMap = array();
+
+        if ( !isset( $contentTypeIdIdentifierMap[$contentTypeId] ) )
+        {
+            $contentTypeService = $this->getContainer()->get( 'ezpublish.api.service.content_type' );
+            $contentTypeIdIdentifierMap[$contentTypeId] = $contentTypeService->loadContentType( $contentTypeId )->identifier;
+        }
+
+        return $contentTypeIdIdentifierMap[$contentTypeId];
+    }
+}
